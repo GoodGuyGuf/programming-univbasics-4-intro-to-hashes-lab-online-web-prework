418d97f15630b48b9b5bdfe92fb36ac6c60754c1
Ethan <ethan_gustafson@outlook.com>
2019-10-28 03:22:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,7 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  [a_value, age: 31, name: "Grace Hopper"]
+  ["Steve", age: 31, name: "Grace Hopper"]
 end
 
 def update_counting_hash(hash, key)
