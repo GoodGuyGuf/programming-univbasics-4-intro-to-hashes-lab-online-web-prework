a207783a4172449334aac4a9ec281776d8da6aeb
Ethan <ethan_gustafson@outlook.com>
2019-10-28 03:32:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,8 +28,11 @@ read_from_hash(h, :a)
 read_from_hash(h, :c)
 
 def update_counting_hash(hash, key)
- h = {key => value}
-  [value] + 1
-  
+  if hash.key?(key)
+    hash[key] += 1 
+  else
+    hash[key] = 1
+  end
+  hash
 end
 
